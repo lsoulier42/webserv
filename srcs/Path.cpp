6487a8671d7c92460b49872f89c6c76e723fe791
mdereuse <mdereuse@student.42.fr>
2021-04-21 13:06:22 +0200

    Path.cpp almost

--- srcs/Path.cpp
+++ srcs/Path.cpp
@@ -6,7 +6,7 @@
 /*   By: mdereuse <mdereuse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 07:15:40 by mdereuse          #+#    #+#             */
-/*   Updated: 2021/04/21 11:22:19 by mdereuse         ###   ########.fr       */
+/*   Updated: 2021/04/21 13:05:20 by mdereuse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -188,6 +188,39 @@ Path::is_user_info(const std::string &path) {
 	return (true);
 }
 
+//TODO:: ameliorer
+bool
+Path::is_hostname(const std::string &path) {
+	size_t	index(0);
+	while (index < path.size())
+		if (!is_alphanum(path[index])
+				&& !(path[index] == '.'
+					|| path[index] == '-'))
+			return (false);
+	return (true);
+}
+
+bool
+Path::is_ipv4_address(const std::string &path) {
+	std::vector<std::string>	segs(Syntax::split(path, "."));
+	if (segs.size() != 4)
+		return (false);
+	int	byte;
+	for (std::vector<std::string>::iterator it(segs.begin()) ; it != segs.end() ; it++) {
+		if (!Syntax::str_is_num(*it))
+			return (false);
+		byte = std::strtol((*it).c_str(), NULL, 10);
+		if (byte < 0 || byte > 255)
+			return (false);
+	}
+	return (true);
+}
+
+bool
+Path::is_host(const std::string &path) {
+	return (is_hostname(path) || is_ipv4_address(path));
+}
+
 bool
 Path::is_hostport(const std::string &path) {
 	std::string	seg(path.substr(0, path.find(":")));
@@ -217,6 +250,30 @@ Path::is_server(const std::string &path) {
 }
 
 bool
+Path::is_reg_name(const std::string &path) {
+	size_t	index(0);
+	if (path.size() == 0)
+		return (false);
+	while (index < path.size()) {
+		if (is_unreserved_path_char(path[index])
+				|| path[index] == '$'
+				|| path[index] == ','
+				|| path[index] == ';'
+				|| path[index] == ':'
+				|| path[index] == '@'
+				|| path[index] == '&'
+				|| path[index] == '='
+				|| path[index] == '+')
+			index++;
+		else if (is_escaped_path_char(path, index))
+			index += 3;
+		else
+			return (false);
+	}
+	return (true);
+}
+
+bool
 Path::is_authority_component(const std::string &path) {
 	return (is_server(path) || is_reg_name(path));
 }
@@ -225,16 +282,71 @@ bool
 Path::is_net_path(const std::string &path) {
 	if (path.size() < 3 || path.compare(0, 2, "//"))
 		return (false);
-	std::string	seg(path.substr(2));
+	std::string seg;
+	size_t		end_authority(path.find("/", 2));
+	if (std::string::npos != end_authority) {
+		seg = path.substr(2, end_authority - 2);
+		if (!is_authority_component(seg))
+			return (false);
+		seg = path.substr(end_authority + 1);
+		return (is_absolute_path(seg));
+	}
+	seg = path.substr(2);
+	return (is_authority_component(seg));
 }
 
 bool
 Path::is_hier_part(const std::string &path) {
-	std::string seg(path.substr(0, path.find("?")));
-	if (!is_net_path(seg) && !is_abs_path(seg))
+	size_t	start_query(path.find("?"));
+	std::string seg(path.substr(0, start_query));
+	if (!is_net_path(seg) && !is_absolute_path(seg))
 		return (false);
-	seg = path.substr(path.find("?") + 1);
-	return (is_query_string(seg));
+	if (std::string::npos != start_query) {
+		seg = path.substr(path.find("?") + 1);
+		return (is_query_string(seg));
+	}
+	return (true);
+}
+
+bool
+Path::is_uric_path_char(const std::string &path, size_t index) {
+	return (is_reserved_path_char(path[index])
+			|| is_unreserved_path_char(path[index])
+			|| is_escaped_path_char(path, index));
+}
+
+bool
+Path::is_uric_no_slash_path_char(const std::string &path, size_t index) {
+	return (is_unreserved_path_char(path[index])
+			|| is_escaped_path_char(path, index)
+			|| path[index] == ';'
+			|| path[index] == '?'
+			|| path[index] == ':'
+			|| path[index] == '@'
+			|| path[index] == '&'
+			|| path[index] == '='
+			|| path[index] == '+'
+			|| path[index] == '$'
+			|| path[index] == ',');
+}
+
+bool
+Path::is_opaque_part(const std::string &path) {
+	if (path.size() == 0 || !is_uric_no_slash_path_char(path, 0))
+		return (false);
+	if (path.size() > 0) {
+		size_t	index(1);
+		while (index < path.size()) {
+			if (is_reserved_path_char(path[index])
+					|| is_unreserved_path_char(path[index]))
+				index++;
+			else if (is_escaped_path_char(path, index))
+				index += 3;
+			else
+				return (false);
+		}
+	}
+	return (true);
 }
 
 bool
@@ -261,11 +373,11 @@ Path::is_local_path_query(const std::string &path) {
 
 bool
 Path::is_fragment_uri(const std::string &path) {
-	std::string	seg(path.substr(0, path.find("#")));
+	size_t	start_fragment(path.find("#"));
+	std::string	seg(path.substr(0, start_fragment));
 	if (!is_absolute_uri(seg))
 		return (false);
-	size_t	start_fragment;
-	if (std::string::npose != (start_fragment = path.find("#"))) {
+	if (std::string::npose != start_fragment) {
 		seg = path.substr(start_fragment + 1);
 		return (is_fragment(seg));
 	}
